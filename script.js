a8ccb8d3c0dff42125106a9ca724d8f9ca57a909
Agam <agam.rai0123@gmail.com>
2024-06-27 23:19:03 +0530

    used sleep instead of settimeout

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 var start = document.getElementById("start");
 var reset = document.getElementById("reset");
+var pause = document.getElementById("pause");
 
 var h = document.getElementById("hour-input");
 var m = document.getElementById("min-input");
 var s = document.getElementById("sec-input");
 
+var pauseCalled = false;
+var resetCalled = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 start.addEventListener("click", function(){
+    pauseCalled = false;
     let hour   = parseInt(h.value) || 0;
     let minute = parseInt(m.value) || 0;
     let second = parseInt(s.value) || 0;
 
     countdown(hour, minute, second);
-
-    console.log(hour, minute, second);
+    return;
+    //console.log(hour, minute, second);
 });
 
+pause.addEventListener("click", function(){
+    pauseCalled = true;
+    return;
+})
 
 reset.addEventListener("click", function(){
+    resetCalled = true;
     h.value = null;
     m.value = null;
-    s.value = null;
-
+    s.value = null
     console.log(h.value, m.value, s.value);
+    return;
 });
 
 function toSec(hour, minute, second){
@@ -39,14 +53,17 @@ function validate(hour, minute, second){
     return;
 }
 
-function countdown(hour, minute, second){
+async function countdown(hour, minute, second){
     validate(hour, minute, second);
     let time = toSec(hour,minute,second);
-    console.log(time);
-    for(let i = time; i > 0; i--){
-        setTimeout(() => setTimer(time - i),i*1000);
+    for(let i = time; i >= 0; i--){
+        //setTimeout(() => setTimer(time - i),i*1000);
+        setTimer(i);
+        await sleep(1000);
+        if(pauseCalled){
+            break;
+        }
     }
-
     return;
 }
 
@@ -54,8 +71,10 @@ function setTimer(time){
     h.value = Math.floor(time / 3600);
     m.value = Math.floor((time % 3600) / 60);
     s.value = time % 60;
+    console.log(h.value, m.value, s.value);
     if(time == 0){
-        //alert("Time Up!");
-        window.open("bit.ly/IqT6zt");
+        alert("Time Up!");
+        //window.open("bit.ly/IqT6zt");
     }
+    return;
 }
